b5b1af440d49172144fa12d877b2deeba6ce4441
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-21 15:47:02 -0300

    importando funçao da api

--- front-end/src/pages/ProductAdminInsert.js
+++ front-end/src/pages/ProductAdminInsert.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import SideBarAdmin from '../components/SideBarAdmin/SideBarAdmin';
 // import { hanldeRegisterProduct, handleChange } from '../services/productInsert';
+import { createNewProduct } from '../services/api';
 import './ProductAdminInsert.css';
 
 function ProductAdminInsert() {
